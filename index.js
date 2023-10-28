8c22dfd3ca8e99e1272dc85fa77e2ebea05526f1
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-29 00:25:32 +0300

    added score checker and win on 5 points

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const buttons = document.querySelectorAll("button");
+let userScore = 0;
+let pcScore = 0;
+let h1 = document.createElement("h1");
+let score = document.createElement("h1");
+let result = document.createElement("h1");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -13,18 +18,41 @@ function endGame(pc, user, outcome) {
   } else {
     message = `You picked ${user}. Pc picked ${pc}.${outcome} is the winner`;
   }
-  showWinner = document.getElementById("outcome");
-  showWinner.innerHTML = `<h1>${message}</h1>`;
+  showOutcome = document.querySelector("#outcome");
+  try {
+    showOutcome.removeChild(result);
+  } catch (e) {
+    // attempt to remove result from previous game
+  }
+  h1.textContent = message;
+  showOutcome.appendChild(h1);
+  score.textContent = `You have ${userScore} points. pc has ${pcScore} points.`;
+  showOutcome.appendChild(score);
+  if (pcScore == 5 || userScore == 5) {
+    if (userScore > pcScore) {
+      result.textContent = "You have won";
+      showOutcome.appendChild(result);
+    } else {
+      result.textContent = "Pc has won";
+      showOutcome.appendChild(result);
+    }
+    pcScore = 0;
+    userScore = 0;
+  }
 }
 
 function checkWinner(pc, user) {
   if (user === "rock" && pc === "scissors") {
+    userScore += 1;
     return "user";
   } else if (user === "paper" && pc === "rock") {
+    userScore += 1;
     return "user";
   } else if (user === "scissors" && pc === "paper") {
+    userScore += 1;
     return "user";
   } else {
+    pcScore += 1;
     return "pc";
   }
 }
